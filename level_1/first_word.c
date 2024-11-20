a676ba9f6d23009a3b1885ba1c1e50d5b33ee82d
rapha4x <rapha4.lx@gmail.com>
2024-11-20 12:16:51 -0300

    finish first level 1 question

---
+++ level_1/first_word.c
@@ -0,0 +1,18 @@
+int main(int argc, char **argv)
+{
+	if (argv != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	int i = 0;
+	while (argv[1][i] && argv[1][i] <= 32)
+		i++;
+	while (argv[1][i] && argv[1][i] > 32)
+	{
+		write(1, &argv[1][i], 1);
+		i++;
+	}
+	write (1,"\n", 1);
+	return (0);
+}
