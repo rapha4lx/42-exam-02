30ef965914f9d97a3ac4e529eedc119e3a25b0e9
rapha4x <rapha4.lx@gmail.com>
2024-11-20 13:05:53 -0300

    finish fizzbuzz question

---
+++ level_1/fizzbuzz.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + 48);
+}
+
+int main(void)
+{
+	int i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		ft_putchar('\n');
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
+}
